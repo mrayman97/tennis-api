4619b6a40e9d47edc69bceced98fd37cb4f21702
Ayman JAWADI <paji12251@commun.ad.sncf.fr>
2026-06-12 16:28:45 +0200

    feat(tache-01): endpoint retourne joueurs du meilleur au moins bon

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+import { PlayersService } from './players.service';
 
 @Controller('players')
-export class PlayersController {}
+export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
+	@Get()
+	getAllPlayers() {
+		return this.playersService.getAllPlayers();
+	}
+}
