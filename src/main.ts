99d8e139ed932db820d1d320dbf5387aec5c7e33
Ayman JAWADI <paji12251@commun.ad.sncf.fr>
2026-06-15 14:01:02 +0200

    feat: add swagger and versionning apis

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +15,20 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Tennis Statistics API')
+    .setDescription('REST API exposing tennis players data and statistics.')
+    .setVersion('1.0.0')
+    .addTag('players')
+    .addTag('stats')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 void bootstrap();
